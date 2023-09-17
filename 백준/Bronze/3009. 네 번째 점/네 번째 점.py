e75cf7d87cb4fa14b8c0a5e80f27c7a09d204495
YUNGYEOM KIM <78156719+gyeom-yee@users.noreply.github.com>
2023-09-17 16:53:40 +0900

    [Bronze III] Title: 네 번째 점, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.py
@@ -0,0 +1,14 @@
+import sys
+x_arr = []
+y_arr = []
+for _ in range(3):
+    x, y = map(int, sys.stdin.readline().split())
+    if x in x_arr:
+        x_arr.remove(x)
+    else:
+        x_arr.append(x)
+    if y in y_arr:
+        y_arr.remove(y)
+    else:
+        y_arr.append(y)
+print(x_arr[0],y_arr[0])
