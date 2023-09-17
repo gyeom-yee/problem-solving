6be7a4d8065d4f3f1a8f6bea71f7ed94cccd1dda
YUNGYEOM KIM <78156719+gyeom-yee@users.noreply.github.com>
2023-09-17 16:55:27 +0900

    [Bronze III] Title: 대지, Time: 96 ms, Memory: 35196 KB -BaekjoonHub

---
+++ 백준/Bronze/9063. 대지/대지.py
@@ -0,0 +1,8 @@
+import sys
+x = []
+y = []
+for _ in range(int(input())):
+    a, b = map(int, sys.stdin.readline().split())
+    x.append(a)
+    y.append(b)
+print((max(x)-min(x))*(max(y)-min(y)))
