d4a0c0e432e2efc287842b1f1ff7aaaef32752e0
YUNGYEOM KIM <dbsrua1105@naver.com>
2023-09-24 18:13:19 +0900

    Revert "delete file"
    This reverts commit 41ff951d80905eded3af749f63128e230d4bc800.

---
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
@@ -0,0 +1,9 @@
+from collections import deque
+n, k = map(int, input().split())
+q = deque(range(1, n+1))
+ans = []
+while q:
+    for _ in range(k-1):
+        q.append(q.popleft())
+    ans.append(q.popleft())
+print(f"<{', '.join([str(i) for i in ans]) }>")
