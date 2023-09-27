23bfb0d526dc951b6ffb4ca1a6d0d443191ea6e2
YUNGYEOM KIM <78156719+gyeom-yee@users.noreply.github.com>
2023-09-28 00:41:40 +0900

    Update 붙임성 좋은 총총이.py

--- 백준/Silver/26069. 붙임성 좋은 총총이/붙임성 좋은 총총이.py
+++ 백준/Silver/26069. 붙임성 좋은 총총이/붙임성 좋은 총총이.py
@@ -4,9 +4,7 @@ meet = sys.stdin.readlines()[1:]
 for x in meet:
     a, b = x.split()
     if a in dance:
-        if b in dance: continue
-        else: dance.add(b)
+        dance.add(b)
     elif b in dance:
-        if a in dance: continue
-        else: dance.add(a)
+        dance.add(a)
 print(len(dance))
