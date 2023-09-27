a0a9ba35934d1bd41c6dc4741e3ddd8bb37e416e
YUNGYEOM KIM <78156719+gyeom-yee@users.noreply.github.com>
2023-09-28 00:09:50 +0900

    Update 수 찾기.py

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,10 +1,7 @@
 import sys
 input = sys.stdin.readline
 input()
-n_arr = {x for x in input().split()}
+n_arr = set(input().split())
 input()
 m_arr = input().split()
-ans = []
-for x in m_arr:
-    ans.append("1" if x in n_arr else "0")
-sys.stdout.write("\n".join(ans))
+sys.stdout.write("\n".join(["1" if x in n_arr else "0" for x in m_arr]))
