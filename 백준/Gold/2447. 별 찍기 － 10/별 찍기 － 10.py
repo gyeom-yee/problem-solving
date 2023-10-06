c769757d7f6823f2a744073934274a67c0102af2
YUNGYEOM KIM <78156719+gyeom-yee@users.noreply.github.com>
2023-10-06 14:10:56 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 64 ms, Memory: 40604 KB -BaekjoonHub

---
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.py
@@ -0,0 +1,16 @@
+def star_stamp(n):
+    if n == 1:
+        return ["*"]
+    stars = star_stamp(n//3)
+    ans = []
+    for x in stars:
+        ans.append(x*3)
+    for x in stars:
+        ans.append(x + " "*(n//3) + x)
+    for x in stars:
+        ans.append(x*3)
+    return ans
+
+n = int(input())
+star_stamp(n)
+print("\n".join(star_stamp(n)))
