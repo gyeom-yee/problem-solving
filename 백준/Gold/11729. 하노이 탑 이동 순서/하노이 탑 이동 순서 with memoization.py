7068a255734c055dd175117d30d236f1fa1ec475
YUNGYEOM KIM <78156719+gyeom-yee@users.noreply.github.com>
2023-10-07 16:24:41 +0900

    Create 하노이 탑 이동 순서 with 메모이제이션.py

---
+++ 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서 with memoization.py
@@ -0,0 +1,16 @@
+import sys
+def hanoi(n, from_pos, by_pos, to_pos):
+    key = (n, from_pos, by_pos, to_pos)
+    if key in memo:
+        return memo[key]
+    elif n == 1:
+        return f"{from_pos} {to_pos}"
+    tmp = "\n".join([hanoi(n-1, from_pos, to_pos, by_pos), f"{from_pos} {to_pos}", hanoi(n-1, by_pos, from_pos, to_pos)])
+    memo[key] = tmp
+    return tmp
+
+ans = []
+memo = dict()
+n = int(input())
+print(2**n -1)
+sys.stdout.write(hanoi(n, 1, 2, 3))
