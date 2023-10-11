dc3d2fe1099f2d76b2bea89c986e6397f54ded0d
YUNGYEOM KIM <78156719+gyeom-yee@users.noreply.github.com>
2023-10-12 00:07:34 +0900

    [Gold IV] Title: N-Queen, Time: 29596 ms, Memory: 199140 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.py
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -1,2 +1,21 @@
-ans = [0, 1, 0, 0, 2, 10, 4, 40, 92, 352, 724, 2680, 14200, 73712, 365596, 2279184]
-print(ans[int(input())])
+def dfs(x):
+    global ans
+    if x == n:
+        ans += 1
+        return
+    else:
+        for i in range(n):
+            row[x] = i
+            if isPossible(x):
+                dfs(x+1)
+
+def isPossible(x):
+    for i in range(x):
+        if row[x] == row[i] or abs(row[x] - row[i]) == x-i:
+            return False
+    return True
+n = int(input())
+row = [0]*n
+ans = 0
+dfs(0)
+print(ans)
