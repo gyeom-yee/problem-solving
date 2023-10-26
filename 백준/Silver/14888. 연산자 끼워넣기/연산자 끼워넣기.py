f9f439f81dc6e92360be3f4165303c4708229ecb
YUNGYEOM KIM <78156719+gyeom-yee@users.noreply.github.com>
2023-10-26 14:47:23 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 1080 ms, Memory: 121228 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -1,28 +1,31 @@
+from itertools import permutations
+def calculator(x, arr):
+    tmp = arr[0]
+    for op, val in zip(x, arr[1:]):
+        if op == '+': tmp += val
+        elif op == '-': tmp -= val
+        elif op == '*': tmp *= val
+        elif op == '/':
+            if tmp < 0:
+                tmp = -(-tmp//val)
+            else:
+                tmp //= val
+    return tmp
+
 n = int(input())
 arr = list(map(int, input().split()))
-add, sub, mul, div = map(int, input().split())
+operator = list(map(int, input().split()))
+operator_li = []
+memo = dict()
+ans = []
 
-minnum, maxnum = 1e10, -1e10
-
-def dfs(i, ans, add, sub, mul, div):
-    global minnum, maxnum
-    if i == n:
-        maxnum = max(maxnum, ans)
-        minnum = min(minnum, ans)
-        return
-
+for idx, op in zip(operator, ['+','-','*','/']):
+    if idx: operator_li.extend(idx*op)
+for x in permutations(operator_li, n-1):
+    if x in memo:
+        continue
     else:
-        if add:
-            dfs(i+1, ans+arr[i], add-1, sub, mul, div)
-        if sub:
-            dfs(i+1, ans-arr[i], add, sub-1, mul, div)
-        if mul:
-            dfs(i+1, ans*arr[i], add, sub, mul-1, div)
-        if div:
-            if ans < 0:
-                dfs(i+1, -(-ans//arr[i]), add, sub, mul, div-1)
-            else:
-                dfs(i+1, ans//arr[i], add, sub, mul, div-1)
-
-dfs(1, arr[0], add, sub, mul, div)
-print(maxnum, minnum, sep='\n')
+        tmp = calculator(x, arr)
+        memo[x] = tmp
+        ans.append(tmp)
+print(max(ans), min(ans), sep='\n')
