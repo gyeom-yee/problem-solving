612b38fa7c6b792fce139cea413c4750fccaa160
YUNGYEOM KIM <78156719+gyeom-yee@users.noreply.github.com>
2024-01-03 11:16:42 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -0,0 +1,12 @@
+def func(x):
+    if x == 1:
+        return 1
+    elif x == 2:
+        return 2
+    elif x == 3:
+        return 4
+    else:
+        return func(x-1)+func(x-2)+func(x-3)
+
+for _ in range(int(input())):
+    print(func(int(input())))
