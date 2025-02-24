dc4b4e404bb5b6d472866681ea6f8a5ab16ddde9
YUNGYEOM KIM <78156719+gyeom-yee@users.noreply.github.com>
2025-02-24 22:00:35 +0900

    [Silver III] Title: N과 M (5), Time: 44 ms, Memory: 36980 KB -BaekjoonHub

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.py
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.py
@@ -1,16 +1,4 @@
+from itertools import permutations
 n, m = map(int, input().split())
-n_li = sorted(list(map(int, input().split())))
-used = [0]*n
-
-def perm(arr, r):
-    if r == m:
-        print(*arr)
-        return
-    for i in range(n):
-        if not used[i]:
-            used[i] = 1
-            arr.append(n_li[i])
-            perm(arr, r+1)
-            arr.pop()
-            used[i] = 0
-perm([], 0)
+n_li = sorted(list(input().split()), key=int)
+print('\n'.join(map(" ".join, permutations(n_li, m))))
