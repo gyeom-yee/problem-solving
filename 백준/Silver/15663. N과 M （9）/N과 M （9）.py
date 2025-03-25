4e12eae37329d0f99812e0e75a427bcfc3d71fdd
YUNGYEOM KIM <78156719+gyeom-yee@users.noreply.github.com>
2025-03-25 20:56:33 +0900

    [Silver II] Title: N과 M (9), Time: 60 ms, Memory: 37024 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.py
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.py
@@ -1,6 +1,10 @@
 from itertools import permutations
 n, m = map(int, input().split())
-arr = input().split()
-result = sorted(set(tuple(map(int, item)) for item in permutations(arr, m)))
-for item in result:
-    print(" ".join(map(str,item)))
+arr = sorted(input().split(), key=int)
+result = set()
+
+for item in permutations(arr, m):
+    s = " ".join(item)
+    if s not in result:
+        result.add(s)
+        print(s)
