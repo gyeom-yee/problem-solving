5f00dc92d06452a9e66c152534438d060ba92478
YUNGYEOM KIM <78156719+gyeom-yee@users.noreply.github.com>
2025-04-03 11:14:27 +0900

    [Silver IV] Title: GCD 합, Time: 300 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/9613. GCD 합/GCD 합.py
@@ -0,0 +1,16 @@
+import sys
+from itertools import combinations
+input = sys.stdin.readline
+
+def gcd(a, b):
+    while b != 0:
+        a, b = b, a%b
+    return a
+
+n = int(input())
+for _ in range(n):
+    result = 0
+    arr = sorted(list(map(int, input().split()))[1:], reverse=True)
+    for twin in combinations(arr, 2):
+        result += gcd(twin[0], twin[1])
+    print(result)
