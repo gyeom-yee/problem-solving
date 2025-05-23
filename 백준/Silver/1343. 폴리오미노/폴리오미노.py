8d46b3ced02d36b961c2017a3d7bd94eac0d8947
YUNGYEOM KIM <78156719+gyeom-yee@users.noreply.github.com>
2025-05-23 15:27:56 +0900

    [Silver V] Title: 폴리오미노, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1343. 폴리오미노/폴리오미노.py
+++ 백준/Silver/1343. 폴리오미노/폴리오미노.py
@@ -1,16 +1,2 @@
-import re
-board = re.findall(r'X+|\.+', input())
-res = []
-flag = 0
-
-for i in range(len(board)):
-    n = len(board[i])
-    if 'X' in board[i]:
-        if n < 2 or n%2 != 0:
-            flag = 1
-            break
-        if n%4 == 0:
-            board[i] = (n//4)*'AAAA'
-        elif n%2 == 0:
-            board[i] = (n//4)*'AAAA'+(n%4//2)*'BB'
-print(-1 if flag else "".join(board))
+board = input().replace('XXXX', 'AAAA').replace('XX', 'BB')
+print(-1 if 'X' in board else board)
