4b329f8667bea19227cf3658d07d28087ac894c1
YUNGYEOM KIM <78156719+gyeom-yee@users.noreply.github.com>
2025-06-26 23:49:13 +0900

    [Bronze I] Title: 0의 개수, Time: 364 ms, Memory: 104420 KB -BaekjoonHub

--- 백준/Bronze/11170. 0의 개수/0의 개수.py
+++ 백준/Bronze/11170. 0의 개수/0의 개수.py
@@ -1,4 +1,8 @@
 t = int(input())
+arr = list(map(str, range(0, 1_000_001)))
+zero = [0]*1_000_001
+for i in range(1_000_001):
+    zero[i] = arr[i].count('0')
 for _ in range(t):
     start, end = map(int, input().split())
-    print("".join(map(str, range(start, end+1))).count('0'))
+    print(sum(zero[start:end+1]))
