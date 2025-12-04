da104cb104085734da47da9732745a149731aa68
YUNGYEOM KIM <78156719+gyeom-yee@users.noreply.github.com>
2025-12-05 00:12:55 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.05 ms, Memory: 9.24 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/README.md
+++ 프로그래머스/1/12943. 콜라츠 추측/README.md
@@ -1,10 +1,10 @@
 # [level 1] 콜라츠 추측 - 12943 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12943) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12943?language=python3) 
 
 ### 성능 요약
 
-메모리: 9.11 MB, 시간: 0.07 ms
+메모리: 9.24 MB, 시간: 0.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 12월 05일 00:08:44
+2025년 12월 05일 00:12:52
 
 ### 문제 설명
 
--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -1,15 +1,14 @@
 def solution(num):
     answer = 0
-    cnt = 0
     
     if num == 1:
         pass
     else:
-        while num != 1 and cnt < 500:
-            cnt += 1
+        for i in range(1, 501):
             if num%2 == 0: 
                 num //= 2
+                if num == 1: break
             else:
                 num = num*3 + 1
-        answer = (cnt if num==1 else -1)
+        answer = (i if num==1 else -1)
     return answer
