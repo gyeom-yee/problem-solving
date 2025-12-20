0674840ca1328d36996bb59a17c7f76038c19007
YUNGYEOM KIM <78156719+gyeom-yee@users.noreply.github.com>
2025-12-20 14:47:21 +0900

    [level 1] Title: 약수의 합, Time: 0.02 ms, Memory: 9.32 MB -BaekjoonHub

---
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.py
@@ -0,0 +1,8 @@
+def solution(n):
+    result = []
+    for i in range(1, int(n**0.5+1)):
+        if n%i==0:
+            result.append(i)
+            if i != n//i:
+                result.append(n//i)
+    return sum(result)
