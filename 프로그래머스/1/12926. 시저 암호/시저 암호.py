c089fe04bd3bafce19eaf2e609159930206a9be7
YUNGYEOM KIM <78156719+gyeom-yee@users.noreply.github.com>
2025-12-26 14:56:26 +0900

    [level 1] Title: 시저 암호, Time: 1.94 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.py
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.py
@@ -1,17 +1,10 @@
 def solution(s, n):
     answer = ""
     for c in s:
-        temp = ord(c)
-        if c == ' ': answer += ' '
+        if c.isupper():
+            answer += chr((ord(c)-ord('A')+n)%26 + ord('A'))
+        elif c.islower():
+            answer += chr((ord(c)-ord('a')+n)%26 + ord('a'))
         else:
-            if ord('A') <= temp <= ord('Z'):
-                if temp+n <= ord('Z'):
-                    answer += chr(temp+n)
-                else:
-                    answer += chr(temp+n-ord('Z')+ord('A')-1)
-            elif ord('a') <= temp <= ord('z'):
-                if temp+n <= ord('z'):
-                    answer += chr(temp+n)
-                else:
-                    answer += chr(temp+n-ord('z')+ord('a')-1)
+            answer += ' '
     return answer
