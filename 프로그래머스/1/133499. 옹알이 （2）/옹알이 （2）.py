11d99885cb67185ed869f5cbc4a4d0134ca84f46
YUNGYEOM KIM <78156719+gyeom-yee@users.noreply.github.com>
2026-01-02 17:22:36 +0900

    [level 1] Title: 옹알이 (2), Time: 0.19 ms, Memory: 8.97 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.py
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.py
@@ -1,11 +1,8 @@
 def solution(babbling):
     answer = 0
     for word in babbling:
-        word = word.replace("aya", "1").replace("ye", "2").replace("woo", "3").replace("ma", "4")
-        if word.isdigit():
-            for i in range(1, len(word)):
-                if word[i-1] == word[i]:
-                    break
-            else: answer += 1
-        else: pass
+        for c in ["aya", "ye", "woo", "ma"]:
+            if c*2 not in word:
+                word = word.replace(c, " ")
+        if not word.strip(): answer += 1
     return answer
