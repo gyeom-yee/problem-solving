498b67b6c64727b11833db3dafdb44f9fe1a04a9
YUNGYEOM KIM <78156719+gyeom-yee@users.noreply.github.com>
2026-01-07 12:49:07 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.23 ms, Memory: 9.49 MB -BaekjoonHub

--- 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.py
+++ 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.py
@@ -2,16 +2,11 @@ def solution(dartResult):
     import re
     result = re.findall(r'(\d+)([SDT])([*#]?)', dartResult)
     bonus_dict = {'S':1, 'D':2, 'T':3}
+    option_dict = {'':1, '*':2, '#':-1}
     scores = []
     
     for s, b, o in result:
-        temp = int(s) ** bonus_dict[b]
-        
-        if o == '*':
-            temp *= 2
-            if scores:
-                scores[-1] *= 2
-        elif o == '#':
-            temp *= -1
+        temp = (int(s) ** bonus_dict[b]) * option_dict[o]
+        if o == '*' and scores: scores[-1] *= 2
         scores.append(temp)
     return sum(scores)
